f8b6a6dc1ee337448d52cd502951969a24989f16
devdivaysharma11@gmail.com <devdivaysharma11@gmail.com>
2020-01-19 00:01:24 -0800

    intial setup

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './layout/dashboard/dashboard.component';
+import { OrdersComponent } from './layout/orders/orders.component';
+import { PaymentComponent } from './layout/payment/payment.component';
+import { ProductsComponent } from './layout/products/products.component';
+import { UsersComponent } from './layout/users/users.component';
+import { HomeComponent } from './layout/home/home.component';
+
+const routes: Routes = [
+    {path : 'home', component: HomeComponent},
+    {path : 'order', component: OrdersComponent},
+    {path : 'payment', component: PaymentComponent},
+    {path : 'product', component: ProductsComponent},
+    {path : 'user', component: UsersComponent},
+    {path : '**', component: HomeComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './layout/login/login.component';
+import { ProductsComponent } from './layout/products/products.component';
+import { OrdersComponent } from './layout/orders/orders.component';
+import { PaymentComponent } from './layout/payment/payment.component';
+import { UsersComponent } from './layout/users/users.component';
+import { DashboardComponent } from './layout/dashboard/dashboard.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { HomeComponent } from './layout/home/home.component';
+import { SidebarComponent } from './layout/sidebar/sidebar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    ProductsComponent,
+    OrdersComponent,
+    PaymentComponent,
+    UsersComponent,
+    DashboardComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    SidebarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
