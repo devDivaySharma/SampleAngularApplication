91c7abab049a22016cd9379f55f3bdbdf6080ca8
divay <divay@akeo.no>
2020-02-04 12:01:22 +0530

    changes for media component

---
+++ src/app/material.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+
+import { 
+    MatButtonModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatCardModule
+} from '@angular/material';
